6a37532bb9b850690258503a352b02e34769f8e8
bautidea <juanbautistadeangelis@gmail.com>
2024-02-15 14:23:27 -0300

    Added style for h1 and discount msg in hero element.

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,22 +1,40 @@
 import styles from '../styles';
-import { robot } from '../assets';
+import { robot, discount } from '../assets';
+import GetStarted from './GetStarted';
+
 const Hero = () => {
   return (
-    <div className="flex">
-      <div className="flex flex-col font-poppins font-normal text-white">
-        <h2>20% DISCOUNT FOR 1 MONTH ACCOUNT</h2>
-        <h1>The Next Generation Payment Method</h1>
+    <section className={`flex md:flex flex-col ${styles.paddingY}`}>
+      <div
+        className={`flex-1 ${styles.flexStart} flex-col xl:px-0 sm:px-16 px-6`}
+      >
+        <div className="flex flex-row items-center py-[6px] px-4 bg-discount-gradient rounded-[10px] mb-2">
+          <img src={discount} alt="discount" className="w-[32px] h-[32px]" />
+
+          <p className={`${styles.paragraph} ml-2`}>
+            <span className="text-white">20%</span> DISCOUNT FOR{' '}
+            <span className="text-white">1 MONTH</span> ACCOUNT
+          </p>
+        </div>
+
+        <div className="flex flex-row justify-between items-center w-full">
+          <h1 className="flex-1 font-poppins font-semibold ss:text-[72px] text-[52px] text-white ss:leading-[100px] leading-[75px]">
+            The Next <br className="sm:block hidden" />{' '}
+            <span className="text-gradient">Generation</span> Payment Method
+          </h1>
+
+          <div className="ss:flex hidden md:mr-4">
+            <GetStarted />
+          </div>
+        </div>
         <p>
           Out team of experts uses a methodology to identify the credit cards
           most likely to fit your needs. We examine annual percentage rates,
           annual fees.
         </p>
       </div>
-
-      <img src={robot} alt="robot" />
-
-      
-    </div>
+      <img src={robot} alt="robot" className="w-[30rem] h-full" />
+    </section>
   );
 };
 
