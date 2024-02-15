65994c6a8a074c409c031f58c42b7caf6678e609
bautidea <juanbautistadeangelis@gmail.com>
2024-02-15 08:55:59 -0300

    Starting with Hero component.

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,13 +1,21 @@
-import React from 'react';
-
+import styles from '../styles';
+import { robot } from '../assets';
 const Hero = () => {
   return (
     <div className="flex">
-      <div className="flex flex-col">
+      <div className="flex flex-col font-poppins font-normal text-white">
         <h2>20% DISCOUNT FOR 1 MONTH ACCOUNT</h2>
         <h1>The Next Generation Payment Method</h1>
-        <p>Out team of experts uses</p>
+        <p>
+          Out team of experts uses a methodology to identify the credit cards
+          most likely to fit your needs. We examine annual percentage rates,
+          annual fees.
+        </p>
       </div>
+
+      <img src={robot} alt="robot" />
+
+      
     </div>
   );
 };
